aeb4c36b2bd0a4bed6336d698a179621360b0b84
Thomas Chen <chen.thomas.c@gmail.com>
2015-08-23 18:17:49 -0400

    Move form validation from route to controller

--- app/routes/friends/new.js
+++ app/routes/friends/new.js
@@ -4,20 +4,6 @@ export default Ember.Route.extend({
   model() {
     return this.store.createRecord('friend');
   },
-  isValid: Ember.computed(
-    'model.email',
-    'model.firstName',
-    'model.lastName',
-    'model.twitter',
-    {
-      get() {
-        return !Ember.isEmpty(this.get('model.email')) &&
-          !Ember.isEmpty(this.get('model.firstName')) &&
-          !Ember.isEmpty(this.get('model.lastName')) &&
-          !Ember.isEmpty(this.get('model.twitter'));
-      }
-    }
-  ),
   actions: {
     save() {
       console.log('+-- save action bubbled up to friends new route');
